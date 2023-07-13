eecbfb55222293b414211a50e20f7616f2c4aedc
Hope Halfbreed <hopehalfbreed@yandex.ru>
2023-07-13 18:04:56 +0700

    Шаблонизация

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <router-view/>
+  <Layout>
+    <router-view/>
+  </Layout>
 </template>
 
 <script setup>
+import { defineAsyncComponent } from 'vue';
 import { useUser } from './libs/user';
+import { useRoute } from 'vue-router';
 
+const $route = useRoute()
 const $user = useUser()
+
+const Layout = defineAsyncComponent(() => {
+  return import(`@/layouts/${$route.meta.layout || 'DefaultLayout'}.vue`)
+})
 
 $user.checkSession()
   .then(result => {
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="layout layout--detault">
+    <header>Header</header>
+
+    <main>
+      <slot></slot>
+    </main>
+
+    <footer>Footer</footer>
+  </div>  
+</template>
